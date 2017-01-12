fbcc6aa007876031f26377719d631f27ff5e4f49
duanwalker <theoriginalcpututor@gmail.com>
2017-01-12 10:38:40 -0500

    first commit

---
+++ js/Menu_Script.js
@@ -0,0 +1,12 @@
+﻿var main = function () {
+    //new navigation animation
+    /* Push the body and the nav over by 285px over */
+
+    $(".navbar-brand").click(function () {
+        $('.nav').animate({
+            right: "400px"
+        }, 200);
+    });
+
+}
+$(document).ready(main);
